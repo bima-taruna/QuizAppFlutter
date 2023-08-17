8991070ca52b75da654abbedc3ddff144a22c3ed
mylistrikapp@gmail.com <bimataruna21@gmail.com>
2023-08-17 14:38:19 +0700

    loop dummy data to reusable component

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,3 +1,5 @@
+import 'package:first_project/components/answer_button.dart';
+import 'package:first_project/data/questions.dart';
 import 'package:flutter/material.dart';
 
 class QuestionScreen extends StatefulWidget {
@@ -10,10 +12,24 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  final currentQuestion = questions[0];
+
   @override
   Widget build(context) {
-    return const Center(
-      child: Text("Question Screen"),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The Question...',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 30),
+          ...currentQuestion.answers
+              .map((e) => AnswerButton(answerText: e, onTap: () {}))
+        ],
+      ),
     );
   }
 }
