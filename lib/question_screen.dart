96d5f9ffa6a3ff4e9f69e7f74bfaf317aea491d4
mylistrikapp@gmail.com <bimataruna21@gmail.com>
2023-08-17 15:37:33 +0700

    styling question screen

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -18,17 +18,22 @@ class _QuestionScreenState extends State<QuestionScreen> {
   Widget build(context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'The Question...',
-            style: TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers
-              .map((e) => AnswerButton(answerText: e, onTap: () {}))
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers
+                .map((e) => AnswerButton(answerText: e, onTap: () {}))
+          ],
+        ),
       ),
     );
   }
